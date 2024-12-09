69f3aa40ebcd93f1c620364207cad1cf08fb0629
Naresh-official <naresh01.official@gmail.com>
2024-12-09 22:11:28 +0530

    add : create login and signup pages ui

---
+++ src/app/(site)/login/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function LoginPage() {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const onSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+		setIsLoading(true);
+		// Simulate API call
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		setIsLoading(false);
+	};
+
+	return (
+		<div className="container flex h-screen w-screen items-center justify-center">
+			<div className="mx-auto flex w-[90%] md:w-full flex-col justify-center space-y-6 sm:w-[350px]">
+				<div className="flex flex-col space-y-2 text-center">
+					<h1 className="text-2xl font-semibold tracking-tight">
+						Welcome back
+					</h1>
+					<p className="text-sm text-muted-foreground">
+						Enter your email to sign in to your account
+					</p>
+				</div>
+				<div className="grid gap-6">
+					<form onSubmit={onSubmit}>
+						<div className="grid gap-2">
+							<div className="grid gap-1">
+								<Label className="sr-only" htmlFor="email">
+									Email
+								</Label>
+								<Input
+									id="email"
+									placeholder="name@example.com"
+									type="email"
+									autoCapitalize="none"
+									autoComplete="email"
+									autoCorrect="off"
+									disabled={isLoading}
+								/>
+							</div>
+							<div className="grid gap-1">
+								<Label className="sr-only" htmlFor="password">
+									Password
+								</Label>
+								<Input
+									id="password"
+									placeholder="Password"
+									type="password"
+									autoCapitalize="none"
+									autoCorrect="off"
+									disabled={isLoading}
+								/>
+							</div>
+							<Button type="submit" disabled={isLoading}>
+								{isLoading && (
+									<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+								)}
+								Sign In
+							</Button>
+						</div>
+					</form>
+					<div className="relative">
+						<div className="absolute inset-0 flex items-center">
+							<span className="w-full border-t" />
+						</div>
+						<div className="relative flex justify-center text-xs uppercase">
+							<span className="bg-background px-2 text-muted-foreground">
+								Or continue with
+							</span>
+						</div>
+					</div>
+					<div className="grid grid-cols-2 gap-4">
+						<Button variant="outline" disabled={isLoading}>
+							<Image
+								src="/google.svg"
+								width={20}
+								height={20}
+								alt="Google"
+							/>
+							Google
+						</Button>
+						<Button variant="outline" disabled={isLoading}>
+							<Image
+								src="/github.svg"
+								width={25}
+								height={25}
+								alt="GitHub"
+							/>
+							GitHub
+						</Button>
+					</div>
+				</div>
+				<p className="px-8 text-center text-sm text-muted-foreground">
+					<Link
+						href="/signup"
+						className="hover:text-brand underline underline-offset-4"
+					>
+						Don&apos;t have an account? Sign Up
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
+}
